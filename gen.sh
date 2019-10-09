f96943ec07a1b932e798d067233292e58b20d23a
Weaverzhu <weaver_zhu@outlook.com>
2019-10-09 20:26:47 +0800

    pandoc

---
+++ gen.sh
@@ -0,0 +1,3 @@
+pandoc --template algo --filter pandoc/minted.py --pdf-engine=xelatex --no-highlight --pdf-engine-opt="-shell-escape" -o template.tex --from markdown -V monofont="Ubuntu Mono" -V secnumdepth=2 -V --number-sections --toc -V include-before="\renewcommand\labelitemi{$\bullet$}" -V header-includes="\usepackage{minted}" -V geometry="margin=2cm" *-*.md
+latexmk -xelatex -shell-escape template.tex
+latexmk -c
